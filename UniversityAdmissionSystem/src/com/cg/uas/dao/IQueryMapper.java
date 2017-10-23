720e0069f91e5ada2ca05caa2bc70a3b3f8129e1
capgcorp <33022963+capgcorp@users.noreply.github.com>
2017-10-23 15:01:21 +0530

    Add files via upload

---
+++ UniversityAdmissionSystem/src/com/cg/uas/dao/IQueryMapper.java
@@ -0,0 +1,19 @@
+package com.cg.uas.dao;
+
+public interface IQueryMapper {
+	public static final String ADD_PROG_OFFERED = "";
+	public static final String DEL_PROG_OFFERED = "";
+	public static final String UPD_PROG_OFFERED = "";
+	
+	public static final String ADD_PROG_SCHEDULED = "";
+	public static final String DEL_PROG_SCHEDULED = "";
+	public static final String UPD_PROG_SCHEDULED = "";
+	
+	public static final String VIEW_ALL_APPLICANTS = "";
+	public static final String VIEW_PROGS_TIME_PERIOD = "";
+	
+	public static final String LOGIN_VERIFY_ADMIN = "";
+	public static final String LOGIN_VERIFY_MAC = "";
+	
+	
+}
